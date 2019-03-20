88b5069b6c9269e199ac46aa469b70bbdcbf46a7
yauheni-daniliuk <eugene.daniliuk@gmail.com>
2019-03-20 23:50:44 +0300

    feat: optimize solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution 
   let a = bracketsConfig.length;
-  let strNew = str.split('');
-  let b = strNew.length;
+  let b = str.length;
   let stack = [];
 
   for (let k = 0; k < b; k++) {
-    stack.push(strNew[k]);
+    stack.push(str[k]);
     for (let i = 0; i < a; i++) {
-      if (bracketsConfig[i][1] == stack[stack.length - 1] && bracketsConfig[i][0] == stack[stack.length - 2]) {
+      if (bracketsConfig[i][1] == stack[stack.length - 1] 
+        && bracketsConfig[i][0] == stack[stack.length - 2]) {
           stack.splice(stack.length - 2, 2);
       }
     }
