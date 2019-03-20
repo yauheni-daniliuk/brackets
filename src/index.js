f9b6ea81a8a420f140ca426f53767e500e4552b7
yauheni-daniliuk <eugene.daniliuk@gmail.com>
2019-03-20 23:17:47 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // your solution 
+  let a = bracketsConfig.length;
+  let strNew = str.split('');
+  let b = strNew.length;
+  let stack = [];
+
+  for (let k = 0; k < b; k++) {
+    stack.push(strNew[k]);
+    for (let i = 0; i < a; i++) {
+      if (bracketsConfig[i][1] == stack[stack.length - 1] && bracketsConfig[i][0] == stack[stack.length - 2]) {
+          stack.splice(stack.length - 2, 2);
+      }
+    }
+  }
+
+  let result = (stack.length == 0);
+  return result;
 }
